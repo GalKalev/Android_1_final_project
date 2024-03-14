104b1166df67758a7cd748640af0b4800ecc1c78
Gal <galk2508@gmail.com>
2024-03-14 15:25:21 +0200

    starting to frame the structure of the project

---
+++ app/src/main/java/com/example/finalprojectandroid1/activities/LoginSignInActivity.java
@@ -0,0 +1,16 @@
+package com.example.finalprojectandroid1.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.finalprojectandroid1.R;
+
+public class LoginSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_sign_in);
+    }
+}
---
+++ app/src/main/java/com/example/finalprojectandroid1/user/UserInfo.java
@@ -0,0 +1,60 @@
+package com.example.finalprojectandroid1.user;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserInfo {
+    private static Map<String, Map.Entry<String,String>> usersInfo = new HashMap<>();
+
+    private String userEmail;
+    private String userPassword;
+    private String userPhone;
+    private String userName;
+
+    public UserInfo(){
+
+    }
+
+    public UserInfo(String userEmail, String userPassword, String userPhone, String userName) {
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userPhone = userPhone;
+        this.userName = userName;
+
+    }
+
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+
+    @Override
+    public String toString() {
+        return "UserInfo{" +
+
+                ", userEmail='" + userEmail + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", userPhone='" + userPhone + '\'' +
+                '}';
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+}
